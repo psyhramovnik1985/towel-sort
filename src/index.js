07d5ca88ddb133cb5aaa0ac8b1e935e3a889f004
psyhramovnik1985 <psyhramovnik@rambler.ru>
2021-03-12 13:30:34 +0300

    feat: add logics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!Array.isArray(matrix)) {return [];}
+  if (matrix.length === 0) {return [];}
+
+  let mas = [];
+  for (var i = 0; i < matrix.length; i++) {
+    if (i%2 !== 0) {
+      matrix[i].reverse();
+    }
+    mas = mas.concat(matrix[i]);       
+  }
+  return mas;
 }
